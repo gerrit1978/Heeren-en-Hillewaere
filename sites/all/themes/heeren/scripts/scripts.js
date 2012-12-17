dd575ea227f37ea9927d9378f78f1f7bfeaac8f7
Gerrit Vos <gerrit@hedgecomm.be>
2012-12-17 13:36:52 +0100

    Finishen offerteformulier closes #6

--- sites/all/themes/heeren/scripts/scripts.js
+++ sites/all/themes/heeren/scripts/scripts.js
@@ -5,44 +5,17 @@ jQuery(document).ready(function($) {
     e.preventDefault();
     $(this).parent().find('ul').toggle();
   });
-  
-/*
-  // contact form
-  $('#block-webform-client-block-17 .webform-client-form .button.next').click(function(e) {
-    e.preventDefault();
-		// hide "Persoonlijke Gegevens" fieldset
-		$('#block-webform-client-block-17 .webform-client-form #webform-component-persoonlijke-gegevens').fadeOut("fast", function() {
-			// show "Auto Gegevens" fieldset
-			$('#block-webform-client-block-17 .webform-client-form #webform-component-auto-gegevens').fadeIn("fast");
-			// show Submit button
-			$('#block-webform-client-block-17 .webform-client-form #edit-actions').show();
-			// swap tab bg color
-			$('#block-webform-client-block-17 .tabs ul li.auto-gegevens').css('background-color', '#f4f4f4');
-			$('#block-webform-client-block-17 .tabs ul li.persoonlijke-gegevens').css('background-color', '#cfcfcf');			
-			// scroll to Title			
-			var offset = $('#block-webform-client-block-17 h2.node-title').offset();
-			$('html, body').animate({scrollTop:offset.top}, 0);
-		});
-  });
-
-  $('#block-webform-client-block-17 .webform-client-form .button.previous').click(function(e) {
-    e.preventDefault();
-    // hide "Auto Gegevens" fieldset
-		$('#block-webform-client-block-17 .webform-client-form #webform-component-auto-gegevens').fadeOut("fast", function() {
-			// show "Persoonlijke Gegevens" fieldset
-			$('#block-webform-client-block-17 .webform-client-form #webform-component-persoonlijke-gegevens').fadeIn("fast");
-			// hide Submit button
-			$('#block-webform-client-block-17 .webform-client-form #edit-actions').hide();
-			// swap tab bg color
-			$('#block-webform-client-block-17 .tabs ul li.auto-gegevens').css('background-color', '#cfcfcf');
-			$('#block-webform-client-block-17 .tabs ul li.persoonlijke-gegevens').css('background-color', '#f4f4f4');			
-			// scroll to Title
-			var offset = $('#block-webform-client-block-17 h2.node-title').offset();
-			$('html, body').animate({scrollTop:offset.top}, 0);
-
-		});
-  });
-*/
 
 
+  // offerte formulier voertuigen: "active" tab en scroll top positie
+  if ($('#webform-component-auto-gegevens').length) {
+    $('#block-webform-client-block-17 .tabs ul li.auto-gegevens').css('background-color', '#f4f4f4');
+    $('#block-webform-client-block-17 .tabs ul li.persoonlijke-gegevens').css('background-color', '#cfcfcf');    
+		// scroll to Title			
+		var offset = $('#block-webform-client-block-17 h2.node-title').offset();
+		$('html, body').animate({scrollTop:offset.top}, 0);
+  } else {
+    $('#block-webform-client-block-17 .tabs ul li.auto-gegevens').css('background-color', '#cfcfcf');
+    $('#block-webform-client-block-17 .tabs ul li.persoonlijke-gegevens').css('background-color', '#f4f4f4');    
+  }
 });
